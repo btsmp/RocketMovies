b402eacf8849e468cba47469850dde8c51d6b247
Bruno Sampaio <brunoctsb@gmail.com>
2022-08-25 16:13:12 -0300

    inital commit

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Bruno Sampaio</h1>
+      <p>Hello World!</p>
+    </Container>
+  );
+}
