4a99996b4d18629fe77cedde23be05f814538c76
Bruno Sampaio <brunoctsb@gmail.com>
2022-08-25 21:35:40 -0300

    -

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,42 @@
-import { Container } from "./styles";
+import { Container, Links, Content } from "./styles";
 
 import { Header } from "../../components/header";
 import { Button } from "../../components/Button";
+import { ButtonText } from "../../components/ButtonText";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <Button title='Voltar' />
-      
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
+
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit rem sint neque doloribus, tempore nemo nobis voluptatum debitis at perspiciatis. Ipsam, dolorem fuga similique aspernatur quaerat quia iste dolores? Asperiores.</p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">www.rocketseat.com.com.br</a></li>
+              <li><a href="#">www.rocketseat.com.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
+
+          <Button title='Voltar' />
+
+        </Content>
+      </main>
+
+
     </Container>
   );
 }
+
